17ecfda5b3061732ad226d82615135a31908a145
MdNazmul12 <rifathosen7171@gmail.com>
2023-08-20 21:21:25 +0600

    first commit

--- js/index.js
+++ js/index.js
@@ -10,4 +10,26 @@ selectedItemContainer.appendChild(li)
 const price=target.parentNode.childNodes[5].innerText.split(" ")[0]
 total=parseFloat(total)+parseFloat(price);
 const firstTotal=document.getElementById('Total').innerText=total;
+
+//  const discount=firstTotal*0.02;
+// //  const discountPrice=document.getElementById('discount').innerText=total;
+// const totalGrid=firstTotal-discount
+// const gridTotal=document.getElementById('Total-grid').innerText=totalGrid;
+
 }
+function makePar(target){
+    document.getElementById('make-btn')
+    
+}
+
+// document.getElementById('make-btn').addEventListener('click',function(){
+// if(total>200){
+//     removeattribute ("disabled")
+//     {
+//         // else{
+//         //     setAttribute ("disabled" "true")};
+//         // }
+        
+// }
+// })
+
